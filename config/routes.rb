22d91a934b5d47bc59769963cc9886d5b3f9928a
eason <acontentedmind@gmail.com>
2021-01-28 17:47:29 +0800

    user_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :users, controller: 'registrations', only:[:create, :edit, :update] do
+    get '/sign_up', action: 'new'
+    get '/home', action: 'index'
+  end
+
+  resource :users, controller: 'sessions', only:[] do
+    get '/sign_in', action: 'new'
+    post '/sign_in', action: 'create'
+    delete '/logout', action: 'destroy'
+  end
 end
